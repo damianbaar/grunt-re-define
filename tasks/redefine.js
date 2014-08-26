63c9e4566d7ad22377d452e29fa19afc346630c1
Damian Baar <damian.baar@gmail.com>
2014-08-26 21:15:00 +0200

    removed comments

--- tasks/redefine.js
+++ tasks/redefine.js
@@ -11,33 +11,6 @@ module.exports = function(grunt) {
       , config = _.merge(_.omit(this.data, 'files'), this.options())
       , base = config.base
 
-    config.name = this.target
-
-  // var options = 
-  //   { base           : program.base
-  //   , main           : program.main
-  //   , wrapper        : program.wrapper
-  //   , returns        : program.returns
-  //   , names          : program.names
-  //   , excludeDepsRef : program.excludeDepsRef
-  //   , globals        : program.globals
-  //   }
-  //
-  // //CUSTOM TRANSFORMS
-  // var findExternal = require('re-define-include-external')({
-  //     external     : program.external || []
-  //   , discoverable : program.discoverable || ['node_modules', 'bower_component']
-  //   , descriptors  : program.descriptors || ['package.json', 'bower.json']
-  //   , skip         : program.skip
-  //   })
-
-
-
-    // if(config.excludeDeps) config.excludeDeps = config.externals.split(',')
-    // if(config.map)         config.map = _.map(config.map, function(k,v) { return v + '#' + k })
-    // if(config.external)    config.include = _.map(config.external, function(k,v) { return k + '#' + v})
-    //
-    //
     var bundle = redefine
       .bundle(redefine.config(_.omit(config, 'transforms')), config.transforms)
 
@@ -48,27 +21,5 @@ module.exports = function(grunt) {
     }))
 
     bundle.write(new File({path: path.resolve(config.main), cwd: config.base}))
-    //
-    // this.files.forEach(function(f) {
-    //   if(!base) config.base = f.orig.cwd
-    //
-    //   var converter = redefine.fromPath(config)
-    //
-    //   converter
-    //     .pipe(through(function(result, enc, next) {
-    //       grunt.log.writeln('File "' + f.dest + '" created.')
-    //       grunt.file.write(f.dest, result)
-    //       done()
-    //     }))
-    //
-    //   f.src.forEach(function(filepath) {
-    //     var fp = path.resolve(config.base, filepath)
-    //
-    //     if (!grunt.file.exists(fp)) grunt.log.warn('Source file "' + fp + '" not found.')
-    //     else converter.write({path: fp})
-    //   })
-    //
-    //   converter.end()
-    // })
   })
 }
